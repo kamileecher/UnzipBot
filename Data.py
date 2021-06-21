72abac7b37a2186c965c630cdcdd003a6d34764a
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-06-21 21:55:00 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,35 +5,35 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     # Start Message
-    START = "Hey {}. \n\nWelcome to {} \n\nI can unzip & unrar files you send me and upload them to our private chat. \nI will also total the contents & number of files."
+    START = "Merhaba {}. \n\nDosya gönderebilirsin.."
 
     if Config.OWNER_ID != 0:
         if Config.OWNER_NAME:
             START += (
-                f"\n\nMy Owner :- [{Config.OWNER_NAME}](tg://user?id={Config.OWNER_ID})"
+                f"\n\nSahip :- [{Config.OWNER_NAME}](tg://user?id={Config.OWNER_ID})"
             )
         else:
             print(
                 "You added OWNER_ID but not OWNER_NAME. You need to put both or neithers"
             )
-            print("Quitting the bot")
+            print("Bottan cık")
             raise SystemExit
     else:
-        START += f"\n\nBy @MysteryBots ♥"
+        START += f"\n\nBy @@kamileecher ♥"
 
     # About Message
-    ABOUT = "**About This Bot** \n\nThis is an open source Unzip bot by @MysteryBots \n\nSource : [Click Here](https://github.com/MysteryBots/UnzipBot) \n\nFramework : [Pyrogram](docs.pyrogram.org) \n\nLanguage : [Python](www.python.org) \n\nDeveloper : [Mყʂƚҽɾყ Bσყ](https://t.me/MysteryxD)"
+    ABOUT = "**Haydaa** \n\nNe arıyorsun.. \n\nKanal : [Tıkla](https://t.me/liveeboxx)"
 
     if Config.OWNER_ID != 0:
         if Config.OWNER_NAME:
             ABOUT += (
-                f"\n\nMy Owner :- [{Config.OWNER_NAME}](tg://user?id={Config.OWNER_ID})"
+                f"\n\nSahip :- [{Config.OWNER_NAME}](tg://user?id={Config.OWNER_ID})"
             )
         else:
             print(
                 "You added OWNER_ID but not OWNER_NAME. You need to put both or neither"
             )
-            print("Quitting the bot")
+            print("Bottan cık")
             raise SystemExit
 
     # Deploy Message
