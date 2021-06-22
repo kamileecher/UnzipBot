8c1c1eac5d8f64963de2d7a9dfe89e06d1830ea9
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-06-22 07:33:01 +0300

    Update tortoise.py

--- UnzipBot/tortoise.py
+++ UnzipBot/tortoise.py
@@ -19,12 +19,12 @@ async def _tortoise(unzipbot, callback_query):
     file_name = msg.document.file_name
     file_size = msg.document.file_size
     if file_size > 1524288000:
-        await msg.reply("500 MB'den büyük dosyalara izin verilmez.", quote=True)
+        await msg.reply("Files with size more than 500 MB aren't allowed.", quote=True)
         return
     try:
-        main = await msg.reply("Indiriliyor...", quote=True)
-        file = await msg.download(progress=progress, progress_args=(main, "İndiriliyor..."))
-        await main.edit("Dosyalar cıkarılıyor...")
+        main = await msg.reply("Downloading...", quote=True)
+        file = await msg.download(progress=progress, progress_args=(main, "Downloading..."))
+        await main.edit("Extracting Files...")
         if file_name.endswith(".zip"):
             with zipfile.ZipFile(file, 'r') as zip_ref:
                 contents = zip_ref.namelist()
@@ -35,19 +35,19 @@ async def _tortoise(unzipbot, callback_query):
                 contents = rar_ref.namelist()
                 rar_ref.extractall("downloads")
             dir_name = file.replace(".rar", "")
-        con_msg = await msg.reply("Icerik kontrol ediliyor...", quote=True)
+        con_msg = await msg.reply("Checking Contents for you...", quote=True)
         constr = ""
         for a in contents:
             b = a.replace(f"{dir_name}/", "")
             constr += b + "\n"
-        ans = "**icerik** \n\n" + constr
+        ans = "**Contents** \n\n" + constr
         if len(ans) > 4096:
-            await con_msg.edit("Icerik kontrol ediliyor... \n\ndosya olarak gönderiliyor...")
-            f = open("icerik.txt", "w+")
+            await con_msg.edit("Checking Contents for you... \n\nSending as file...")
+            f = open("contents.txt", "w+")
             f.write(ans)
             f.close()
-            await msg.reply_document("icerik.txt")
-            os.remove("icerik.txt")
+            await msg.reply_document("contents.txt")
+            os.remove("contents.txt")
         else:
             await msg.reply(ans)
         await con_msg.delete()
@@ -56,23 +56,23 @@ async def _tortoise(unzipbot, callback_query):
         for file in extracted_files:
             try:
                 number += 1
-                sending = await msg.reply(f"**Yükleniyor... ({number}/{len(extracted_files)})**")
+                sending = await msg.reply(f"**Uploading... ({number}/{len(extracted_files)})**")
                 await msg.reply_document(file, quote=False, progress=progress,
-                                         progress_args=(sending, f"Yükleniyor... ({number}/{len(extracted_files)})"), disable_notification=True)
+                                         progress_args=(sending, f"Uploading... ({number}/{len(extracted_files)})"), disable_notification=True)
                 await sending.delete()
             except FloodWait as e:
                 time.sleep(e.x)
         stop = datetime.now()
         await msg.reply(
-            f"Cıkarma Basarıyla Tamamlandı..! \n\nİslem {round((stop - start).total_seconds() / 60, 2)} dakika da bitti.. \n\n @kamileecher1")
+            f"Extraction Done Successfully..! \n\nTook {round((stop - start).total_seconds() / 60, 2)} minutes \n\nFor more bots visit @MysteryBots")
     except rarfile.RarCannotExec:
-        await msg.reply("** HATA :** Bu Dosya hata verdi. İçerik çıkarılamıyor..\n\n"
-                        "Bu, bir dosyanın uzantısı manuel olarak değiştirildiğinde olabilir. `.zip`/`.rar` dosya bu biçimde olmasa bile.\n\n"
-                        "Başka bir dosya ile deneyin lütfen.", quote=True
+        await msg.reply("**ERROR :** This File is possibly bugged. Cannot extract content. \n\n"
+                        "This may happen when a file's extension is manually changed to `.zip`/`.rar` even when file isn't in that format. \n\n"
+                        "Try with some other file please.", quote=True
                         )
     except Exception as e:
-        await unzipbot.send_message(msg.chat.id, "**HATA : **" + str(
-            e) + "\n\nBu mesajı @kamileecher1'a iletin de bu sorunu çözün.")
+        await unzipbot.send_message(msg.chat.id, "**ERROR : **" + str(
+            e) + "\n\nForward this message to @MysteryBots too solve this problem.")
     finally:
         if os.path.isdir("downloads"):
             shutil.rmtree("downloads")
