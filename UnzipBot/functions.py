7d0735864c0a9f31279740b0ce4b67d4e1696626
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-06-22 06:54:35 +0300

    Update functions.py

--- UnzipBot/functions.py
+++ UnzipBot/functions.py
@@ -9,7 +9,7 @@ async def progress(current, total, message, process):
     new_total = humanize.naturalsize(total)
     percentage = round((current * 100) / total, 2)
     try:
-        await message.edit(f"**{process}** \n\n**Progress :** {new_current}/{new_total} | {percentage}℅")
+        await message.edit(f"**{process}** \n\n**Ilerleme :** {new_current}/{new_total} | {percentage}℅")
     except FloodWait as e:
         time.sleep(e.x)
 
